1c91908bf47a27eb68369e523ad91059bca55a69
Sebastian <sl5softwarelab@gmail.com>
2018-11-24 11:41:54 +0100

    Update select01.sql

--- Source/sql/template/select01.sql
+++ Source/sql/template/select01.sql
@@ -1,6 +1,6 @@
 SELECT word, worddescription, wordreplacement
      FROM Words
-     WHERE wordindexed GLOB 's%'
+     WHERE wordindexed GLOB upper('s') || '*'
      and   ActionListID > 0
      order by ROWID desc
      LIMIT 9
